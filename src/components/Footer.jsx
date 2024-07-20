422673ce7fc45eb8e493774373fec33a558c09d2
Jayce Turambe <jnn56@txstate.edu>
2024-07-20 16:14:47 -0500

    Integrated jenkins for CI/CD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,10 @@ function Footer() {
       <div className="footer-content">
         <div className="footer-text">
           <p>© 2024 Jayce Turambe Website. All rights reserved.</p>
-          <p>Remember to connect with me!</p>
+          <p>Remember to connect with me! </p>
         </div>
       </div>
+
       <div className="footer-linkedin">
         <a href="https://www.linkedin.com/in/Jayce90" target="_blank" rel="noopener noreferrer">
           LinkedIn
